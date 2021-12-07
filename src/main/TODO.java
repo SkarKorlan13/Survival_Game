1e8fa978496747702d47439869ab1632b482b512
Ellis <Ellis@ceilous.com>
2021-12-06 23:29:57 -0600

    TODO

--- src/main/TODO.java
+++ src/main/TODO.java
@@ -6,7 +6,10 @@ add actual rendering (sprites etc.)
 
 add tiles
 
-
+refactor everything:
+    main initializes window
+        window holds almost everything
+        gamePanel holds graphics and such
 
 
  */
